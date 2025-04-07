f549f8ef1695eca9402792bebf5f777a4f980d5b
nooh-cloud <abdullanooh552@gmail.com>
2025-04-07 19:45:20 +0530

    mondaytask

---
+++ mondaytask/age.js
@@ -0,0 +1,7 @@
+const age=1;
+if(age>=18){
+    console.log("eligible to vote");   
+}
+else if(age<18){
+    console.log("not eligible to vote");
+}
---
+++ mondaytask/equalornot.js
@@ -0,0 +1,10 @@
+const num1 = 5
+const num2 = 7
+
+if(num1 == num2){
+    console.log("these are equal.");
+    
+} else{
+    console.log("these are not equal");
+    
+}
